796ee87ab70dd65330817aa1c1b7279c97b1c073
sathwikgm <sathwikgm14@gmail.com>
2023-06-07 20:40:44 +0530

    updated project component

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -5,13 +5,17 @@ const Projects = () => {
   const projectDetails = [
     {
       name: "Portfolio",
+      img:'src/assets/myportfolio.png',
       github: "https://github.com/SathwikGM/myportfolio",
+      demo: "https://sathwikgm.netlify.app/",
       description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
 
     },
     {
       name: "Tracker",
+      img:'src/assets/myportfolio.png',
       github: "https://github.com/SathwikGM/bug-tracker",
+      demo: "https://sathwikgm.netlify.app/",
       description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     }
   ]
@@ -19,9 +23,15 @@ const Projects = () => {
   const project = projectDetails.map((project) => {
     return (
       <li>
-        <h2>{project.name}</h2>
+        <div>
+          <img src={project.img} alt="" />
+        </div>
+        <div>
+          <h2>{project.name}</h2>
         <p>{ project.description}</p>
-        <a href={project.github}>github</a>
+          <a href={project.github}>github</a>
+          <a href={project.demo}>Demo</a>
+        </div>
       </li>
     )
   });
